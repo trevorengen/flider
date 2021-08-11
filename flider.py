6aca81264a4e0c5e6f35b7c4e71765b0ba0928d8
Trevor Engen <48308407+trevorengen@users.noreply.github.com>
2021-08-10 23:14:02 -0700

    Finish functionality of flider function.

---
+++ flider.py
@@ -0,0 +1,23 @@
+import os
+def flider():
+    try:
+        os.mkdir('flask_app')
+        dirs = ['templates', 'config', 'models', 'controllers', 
+        'static', 'static/css', 'static/js', 'static/img']
+        for dir in dirs:
+            os.mkdir('flask_app/'+dir)
+    except Exception as e:
+        print(e)
+        print('Continuing...')
+    save_paths = ['flask_app/templates/', 'flask_app/config', 'flask_app/', 'flask_app/static/css/', 'flask_app/static/js/']
+    file_names = ['index.html', 'mysqlcontroller.py', '__init__.py', 'style.css', 'scripts.js']
+    for i in range(len(save_paths)):
+        try:
+            completeName = os.path.join(save_paths[i], file_names[i])
+            file = open(completeName, 'w')
+            file.close()
+        except Exception as e:
+            print(e)
+            print('Error on: ' + save_paths[i] + file_names[i])
+    
+flider()
